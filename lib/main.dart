5c535b816f65eb973bbd1d125e8531097875c0cf
satius <sugiyama@supinf.co.jp>
2021-05-22 18:02:40 +0900

    Add url_strategy for better web front experience

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:url_strategy/url_strategy.dart';
 
 import 'app.dart';
 
 void main() {
+  setPathUrlStrategy();
   runApp(ProviderScope(child: App()));
 }
