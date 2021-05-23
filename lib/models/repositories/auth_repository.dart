177ad9c34265f6c65b9375c5570efbd451b67185
satius <sugiyama@supinf.co.jp>
2021-05-24 06:16:08 +0900

    Implement Authentication

---
+++ lib/models/repositories/auth_repository.dart
@@ -0,0 +1,15 @@
+import 'package:shot_client/models/common/result.dart';
+
+abstract class AuthRepository {
+  Future<Result<String>> signUp({
+    required String email,
+    required String password,
+  });
+
+  Future<Result<String>> signIn({
+    required String email,
+    required String password,
+  });
+
+  Future<Result<void>> signOut();
+}
